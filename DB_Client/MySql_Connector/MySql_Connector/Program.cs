83c027d8e588b77a0b00a0e1f329f1ac92ff96bd
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2023-09-22 17:02:26 +0200

    updatz

--- DB_Client/MySql_Connector/MySql_Connector/Program.cs
+++ DB_Client/MySql_Connector/MySql_Connector/Program.cs
@@ -1,31 +1,91 @@
 ﻿using System;
+using System.Data;
 using MySql.Data.MySqlClient;
 
-class Program
+namespace MySql_Connector
 {
-    static void Main()
+    public class MySqlDbManager
     {
-        string connectionString = "server=localhost;user=root;database=warhammer_db";
+        private MySqlConnection connection;
+        private string connectionString;
 
-        MySqlConnection connection = new MySqlConnection(connectionString);
-
-        try
+        public MySqlDbManager(string connectionString)
         {
-            connection.Open();
-            Console.WriteLine("Connexion réussie !");
-
-            // Vous pouvez exécuter vos opérations de base de données ici.
-
-            connection.Close();
+            this.connectionString = connectionString;
+            connection = new MySqlConnection(connectionString);
         }
-        catch (MySqlException ex)
+
+        public void OpenConnection()
         {
-            Console.WriteLine("Erreur de connexion à la base de données : " + ex.Message);
+            try
+            {
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                    Console.WriteLine("Connexion réussie !");
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Erreur lors de l'ouverture de la connexion : " + ex.Message);
+                // Vous pouvez choisir de lever l'exception à nouveau ici ou de gérer l'erreur d'une autre manière.
+            }
         }
-        finally
+
+        public void CloseConnection()
         {
-            connection.Dispose();
+            try
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Erreur lors de la fermeture de la connexion : " + ex.Message);
+                // Vous pouvez choisir de lever l'exception à nouveau ici ou de gérer l'erreur d'une autre manière.
+            }
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                OpenConnection(); // Ouvrir la connexion si ce n'est pas déjà fait.
+
+                using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Erreur lors de l'exécution de la requête : " + ex.Message);
+                // Vous pouvez choisir de lever l'exception à nouveau ici ou de gérer l'erreur d'une autre manière.
+            }
+            finally
+            {
+                CloseConnection(); // Fermer la connexion après l'exécution de la requête.
+            }
+
+            return dataTable;
+        }
+
+        // Ajoutez d'autres méthodes pour effectuer des opérations sur la base de données.
+        static void Main(string[] args)
+        {
+            // Le code que vous souhaitez exécuter en tant qu'application autonome.
+            // Par exemple, vous pouvez créer une instance de MySqlDbManager et effectuer des opérations avec elle.
+            string connectionString = "server=localhost;user=root;database=warhammer_db";
+            MySqlDbManager dbManager = new MySqlDbManager(connectionString);
         }
     }
 }
+
 
