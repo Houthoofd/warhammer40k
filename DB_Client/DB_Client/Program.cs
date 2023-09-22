c4044d90033dabb6cdc8c3da1ef0d43f92bdd775
Guillaume Houthoofd <66558014+Houthoofd@users.noreply.github.com>
2023-09-22 16:32:15 +0200

    update
    update

---
+++ DB_Client/DB_Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MySql_Connector;
+using System.Data;
+
+namespace DB_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "server=localhost;user=root;database=warhammer_db";
+
+            // Créez une instance de MySqlDbManager pour gérer la connexion à la base de données.
+            MySqlDbManager dbManager = new MySqlDbManager(connectionString);
+
+            try
+            {
+                // Ouvrez la connexion à la base de données.
+                dbManager.OpenConnection();
+
+                // Exécutez votre requête SQL. Par exemple, une requête de sélection de toutes les lignes d'une table.
+                string query = "SELECT * FROM test";
+                var result = dbManager.ExecuteQuery(query);
+
+                foreach (DataRow row in result.Rows)
+                {
+                    Console.WriteLine("ID : " + row["id"].ToString());
+                    Console.WriteLine("Nom : " + row["nom"].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erreur : " + ex.Message);
+            }
+            finally
+            {
+                // Fermez la connexion lorsque vous avez terminé.
+                dbManager.CloseConnection();
+            }
+        }
+    }
+}
+
+
+
---
+++ DB_Client/MySql_Connector/MySql_Connector/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "server=localhost;user=root;database=warhammer_db";
+
+        MySqlConnection connection = new MySqlConnection(connectionString);
+
+        try
+        {
+            connection.Open();
+            Console.WriteLine("Connexion réussie !");
+
+            // Vous pouvez exécuter vos opérations de base de données ici.
+
+            connection.Close();
+        }
+        catch (MySqlException ex)
+        {
+            Console.WriteLine("Erreur de connexion à la base de données : " + ex.Message);
+        }
+        finally
+        {
+            connection.Dispose();
+        }
+    }
+}
+
